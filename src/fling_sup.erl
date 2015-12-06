c11d8b8825c4ef0faa1f466d49eed981bb08c30c
Mark Allen <mrallen1@yahoo.com>
2015-12-06 00:40:56 -0600

    Add simple_one_for_one supervisor for ETS server

---
+++ src/fling_sup.erl
@@ -0,0 +1,20 @@
+-module(fling_sup).
+-behaviour(supervisor).
+
+-export([
+	 start_link/0,
+	 start_ets_server/3,
+	 init/1
+	]).
+
+start_link() ->
+    supervisor:start_link({local, fling_sup}, ?MODULE, []).
+
+start_ets_server(GetKey, GetValue, ModName) ->
+    supervisor:start_child(fling_sup, [GetKey, GetValue, ModName]).
+
+init([]) ->
+    SupFlags = {simple_one_for_one, 10, 10},
+    ETS_Server_Spec = {undefined, {fling_ets, start_link, []},
+                temporary, 2000, worker, [fling_ets]},
+    {ok, {SupFlags, [ETS_Server_Spec]}}.
